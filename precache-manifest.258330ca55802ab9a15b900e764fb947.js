512100957031fa9c65948dbc82507f4b0bbfb277
fa1ntx <kamilg7707@gmail.com>
2019-12-11 21:14:51 +0100

    Updates

---
+++ precache-manifest.258330ca55802ab9a15b900e764fb947.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "630c1cd2ed5c6d346786f1a2ad06823c",
+    "url": "/Task-Board/index.html"
+  },
+  {
+    "revision": "1477d3e0076bd7280631",
+    "url": "/Task-Board/static/css/main.c69e6812.chunk.css"
+  },
+  {
+    "revision": "e108196460c09f378c04",
+    "url": "/Task-Board/static/js/2.dc07c8de.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/Task-Board/static/js/2.dc07c8de.chunk.js.LICENSE"
+  },
+  {
+    "revision": "1477d3e0076bd7280631",
+    "url": "/Task-Board/static/js/main.a99f2761.chunk.js"
+  },
+  {
+    "revision": "065e278e5dc5836411da",
+    "url": "/Task-Board/static/js/runtime-main.835996ad.js"
+  }
+]);
